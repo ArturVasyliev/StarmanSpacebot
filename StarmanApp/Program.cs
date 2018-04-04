62fac6e5100cc89378a49b3a877bd962ceda72f0
ArturVasyliev <princeartik@gmail.com>
2018-04-05 01:00:41 +0300

    Tested abilities of telegram bots.

--- StarmanApp/Program.cs
+++ StarmanApp/Program.cs
@@ -6,6 +6,8 @@ using Telegram.Bot.Args;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.InlineKeyboardButtons;
+using Telegram.Bot.Types.InlineQueryResults;
+using Telegram.Bot.Types.InputMessageContents;
 using Telegram.Bot.Types.ReplyMarkups;
 
 namespace StarmanApp
@@ -18,7 +20,18 @@ namespace StarmanApp
         {
             var me = Bot.GetMeAsync().Result;
 
+            
+            Bot.OnCallbackQuery += BotOnCallbackQueryReceived;
+            Bot.OnInlineQuery += BotOnInlineQueryReceived;
+            Bot.OnInlineResultChosen += BotOnInlineResultChosenReceived;
             Bot.OnMessage += BotOnMessageReceived;
+            Bot.OnMessageEdited += BotOnMessageEditedReceived;
+            Bot.OnReceiveError += BotOnReceiveErrorReceived;
+            Bot.OnReceiveGeneralError += BotOnReceiveGeneralErrorReceived;
+            Bot.OnUpdate += BotOnUpdateReceived;
+            
+            
+
             Bot.StartReceiving();
             Console.WriteLine($"Start listening for @{me.Username}");
             Console.ReadLine();
@@ -37,6 +50,8 @@ namespace StarmanApp
 
         private static async void BotOnMessageReceived(object sender, MessageEventArgs messageEventArgs)
         {
+            // Here we should handle buttons and messages
+            Console.WriteLine("BotOnMessageReceived");
             var message = messageEventArgs.Message;
 
             if (message == null || message.Type != MessageType.TextMessage) return;
@@ -84,12 +99,11 @@ namespace StarmanApp
                     break;
 
                 default:
-                    const string usage = @"
-Usage:
-/inline   - send inline keyboard
-/keyboard - send custom keyboard
-/photo    - send a photo
-/request  - request location or contact";
+                    const string usage = @"Usage:
+                                            /inline   - send inline keyboard
+                                            /keyboard - send custom keyboard
+                                            /photo    - send a photo
+                                            /request  - request location or contact";
 
                     await Bot.SendTextMessageAsync(
                         message.Chat.Id,
@@ -97,6 +111,110 @@ Usage:
                         replyMarkup: new ReplyKeyboardRemove());
                     break;
             }
+        }
+
+        private static async void BotOnCallbackQueryReceived(object sender, CallbackQueryEventArgs callbackQueryEventArgs)
+        {
+            // Here we should handle inline buttons
+            Console.WriteLine("BotOnCallbackQueryReceived");
+            var callbackQuery = callbackQueryEventArgs.CallbackQuery;
+
+            await Bot.AnswerCallbackQueryAsync(
+                callbackQuery.Id,
+                $"Received {callbackQuery.Data}");
+
+            await Bot.SendTextMessageAsync(
+                callbackQuery.Message.Chat.Id,
+                $"Received {callbackQuery.Data}");
+        }
+
+        private static async void BotOnInlineQueryReceived(object sender, InlineQueryEventArgs inlineQueryEventArgs)
+        {
+            Console.WriteLine("BotOnInlineQueryReceived");
+            var inlineQuery = inlineQueryEventArgs.InlineQuery;
+
+
+            //id: "1",
+            //        latitude: 40.7058316f,
+            //        longitude: -74.2581888f,
+            //        title: "New York"
+
+
+            //    Latitude: 40.7058316f,
+            //                Longitude: -74.2581888f   // message if result is selected
+
+            InlineQueryResultLocation[] results = new InlineQueryResultLocation[] {
+                new InlineQueryResultLocation()   // displayed result
+                    {
+                        Id = "1",
+                        Latitude = 40.7058316f,
+                        Longitude = -74.2581888f,
+                        Title = "New York",
+                        InputMessageContent = new InputLocationMessageContent()
+                        {
+                            Latitude = 40.7058316f,
+                            Longitude = -74.2581888f
+                        }
+                    },
+
+                new InlineQueryResultLocation()
+                    {
+                        Id = "2",
+                        Latitude = 13.1449577f,
+                        Longitude =  52.507629f,
+                        Title =  "Berlin", // displayed result
+                        InputMessageContent = new InputLocationMessageContent()
+                        {
+                            Latitude = 13.1449577f,
+                            Longitude = 52.507629f   // message if result is selected
+                        }
+                    }
+            };
+
+            await Bot.AnswerInlineQueryAsync(
+                inlineQueryEventArgs.InlineQuery.Id,
+                results,
+                isPersonal: true,
+                cacheTime: 0);
+        }
+
+        private static async void BotOnInlineResultChosenReceived(object sender, ChosenInlineResultEventArgs chosenInlineResultEventArgs)
+        {
+            Console.WriteLine("BotOnInlineResultChosenReceived");
+            var chosenInlineResult = chosenInlineResultEventArgs.ChosenInlineResult;
+        }
+
+        private static async void BotOnMessageEditedReceived(object sender, MessageEventArgs messageEventArgs)
+        {
+            Console.WriteLine("BotOnMessageEditedReceived");
+            var message = messageEventArgs.Message;
+
+            if (message == null || message.Type != MessageType.TextMessage) return;
+
+            switch (message.Text.Split(' ')[0])
+            {
+            }
+        }
+
+        private static async void BotOnReceiveErrorReceived(object sender, ReceiveErrorEventArgs receiveErrorEventArgs)
+        {
+            Console.WriteLine("BotOnReceiveErrorReceived");
+            Console.WriteLine("Received error: {0} — {1}",
+                receiveErrorEventArgs.ApiRequestException.ErrorCode,
+                receiveErrorEventArgs.ApiRequestException.Message);
+            var receiveError = receiveErrorEventArgs.ApiRequestException;
+        }
+
+        private static async void BotOnReceiveGeneralErrorReceived(object sender, ReceiveGeneralErrorEventArgs receiveGeneralErrorEventArgs)
+        {
+            Console.WriteLine("BotOnReceiveGeneralErrorReceived");
+            var receiveGeneralError = receiveGeneralErrorEventArgs.Exception;
+        }
+
+        private static async void BotOnUpdateReceived(object sender, UpdateEventArgs updateEventArgs)
+        {
+            Console.WriteLine("BotOnUpdateReceived");
+            var update = updateEventArgs.Update;
         }
     }
 }
